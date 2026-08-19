c6848264c22c5a17def68b48cc8a36ef34514443
UdAyMaHamud <mahamudkhanuday17528@gmail.com>
2026-08-19 06:32:49 +0600

    Containerize Node.js application

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ app.get("/health", (req, res) => {
     });
 });
 
-app.listen(PORT, "127.0.0.1", () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
 });
